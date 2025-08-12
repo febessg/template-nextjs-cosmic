93673427c3fc5d7e83cae52af450565e0db170d0
febessg <fernanda-bess@hotmail.com>
2025-08-12 17:58:34 -0300

    feat: Integrar componente About dentro do componente Home, encapsulando-o com o Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@ import { Banner } from "@/components/home/banner";
 import styles from "./page.module.css";
 import { HomeProps } from "@/utils/home.type";
 import { getDataHome } from "@/utils/actions/get-data";
+import { Container } from "@/components/container";
+import { About } from "@/components/home/about";
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
   return (
     <div>
       <Banner banner={object.metadata.banner} />
+      <Container>
+        <About />
+      </Container>
     </div>
   );
 }
